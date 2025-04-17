e600c1d02cc15dbf33c71f62dea812838b3a3848
Nitish <snkumar6926@gmail.com>
2025-04-17 18:26:07 +0530

    new changes added

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    // Add the numbers
+    sum = a + b;
+
+    // Output the result
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
