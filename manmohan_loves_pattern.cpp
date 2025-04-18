e2e621aaefa054e935351e5b6d0e3e597628b015
Nitish <snkumar6926@gmail.com>
2025-04-18 19:46:20 +0530

    new changes added

---
+++ manmohan_loves_pattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i) {
+        if (i % 2 != 0) {
+            // Odd row: print '1' i times
+            for (int j = 1; j <= i; ++j) {
+                cout << "1";
+            }
+        } else {
+            // Even row: print '1', then (i-2) zeros, then '1'
+            cout << "1";
+            for (int j = 1; j <= i - 2; ++j) {
+                cout << "0";
+            }
+            cout << "1";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
