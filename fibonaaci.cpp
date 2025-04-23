8687377190779fc22e042b4d40423168ad93329c
Nitish <snkumar6926@gmail.com>
2025-04-23 22:42:25 +0530

    new changes added

--- fibonaaci.cpp
+++ fibonaaci.cpp
@@ -1,19 +1,22 @@
+// Example of fibonacci series in c++...
+
 #include <iostream>
 using namespace std;
 
 int main() {
     int n;
-    
-    cin>>n;
+    cin >> n;
 
     int f_0 = 0, f_1 = 1;
+
     for (int i = 0; i < n; i++) {
-        cout<<f_0<<endl;
+        cout << f_0 << endl;
 
-        //shifting
         int f_2 = f_1 + f_0;
-        f_0 = f_1;
 
-        
+        f_0 = f_1;
+        f_1 = f_2;
     }
+
+    return 0;
 }
